fd9d2be706f92765c538ea6f0a94807df1284294
greggchhavi-prism <gregg.chhavi@gmail.com>
2026-06-24 20:49:51 -0700

    Update get-profile.js

--- netlify/functions/get-profile.js
+++ netlify/functions/get-profile.js
@@ -5,7 +5,7 @@ exports.handler = async function(event) {
   }
   const AIRTABLE_TOKEN = 'pat0wtzilUrgFyWpv.b10873d37fe76a5a5d1646b66bbdaa56a701d197cc81a26afd956091ad4c1626';
   const BASE_ID = 'appDk2mVEQWg0aQTM';
-  const TABLE_ID = 'tblB12L3ROrf1bL0I';
+  const TABLE_ID = 'tblBl2L3ROrf1bL0I';
   const filterFormula = encodeURIComponent(`{PrismType}="${type}"`);
   const url = `https://api.airtable.com/v0/${BASE_ID}/${TABLE_ID}?filterByFormula=${filterFormula}&maxRecords=1`;
   try {
